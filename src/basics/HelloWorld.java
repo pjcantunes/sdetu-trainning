505a635ee438e3e33c1716a5ca32ff90815f8ebc
Paulo <paulo9o@hotmail.com>
2017-11-30 19:32:10 +0100

    Modified HelloWorld

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] arg){
 		System.out.println("Hello Rebe!");
+		System.out.println("I'm just adding code to my project");
 	}
 	
 }
